c77984d1034dd8fa34a2b900d6bb1654d4c4041c
Asus <Doubaracarter@gmail.com>
2025-06-19 22:01:39 +0100

    Redesigned the Application

--- script/app.js
+++ script/app.js
@@ -1,5 +1,4 @@
 const url = 'https://api.spoonacular.com/food/search?apiKey=d521a3c7bc97453ea62414c9d66eaf45';
-// const apiKey = 'd521a3c7bc97453ea62414c9d66eaf45'
 const options = { 
     method: 'GET',  
     headers: {
@@ -11,21 +10,38 @@ let searchbtn = document.getElementById('search-btn');
 let meal = document.getElementById('meal')
 let mealList = document.getElementById('mealList')
 async function getRecipe() {
-    const response = await fetch(url + '&query=' + meal.value, options);
-    const data = await response.json();
-    console.log(data.searchResults[0].results);
+    document.getElementById('loader').style.display = 'block';
+    mealList.innerHTML = '';
 
-    if(mealList.children) {
-        mealList.innerHTML = '';
+    try {
+        const response = await fetch(url + '&query=' + meal.value, options);
+        const data = await response.json();
+
+        const results = data.searchResults?.[0]?.results || [];
+
+        results.forEach(meals => {
+            let div = document.createElement('div');
+            div.innerHTML = `
+            <div class="meal-card">
+              <a target='_blank' class="meal-card-content" href="${meals.link}">
+                <p>${meals.name}</p>
+                <img class="meal-image" src="${meals.image}" alt="${meals.name}" />
+              </a>
+            </div>`;
+            mealList.appendChild(div);
+        });
+
+        if (!results.length) {
+            mealList.innerHTML = "<p>No results found.</p>";
+        }
+    } catch (err) {
+        console.error(err);
+        mealList.innerHTML = "<p>Error fetching data.</p>";
+    } finally {
+        document.getElementById('loader').style.display = 'none';
     }
-    data.searchResults[0].results.forEach(meals => {
-        let li = document.createElement('li');
-        li.innerHTML = `<a target='_blank' href="${meals.link}">
-        <p>${meals.name}<p></a>
-        <a target='_blank' href="${meals.link}"><img src="${meals.image}" alt="${meals.name}"></a>`;
-        mealList.appendChild(li);
-    })
 }
+
 searchbtn.addEventListener('click', getRecipe);
 meal.addEventListener("keyup", function (e) {
     if (e.key == "Enter") {
